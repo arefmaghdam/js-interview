36fd8fdfd8abf7739bd39964f451b8ec2f3e1a1d
arefmaghdam <arefmaghdam@gmail.com>
2023-04-22 18:49:52 +0430

    added js for 009

---
+++ 009.js
@@ -0,0 +1,15 @@
+console.log("Part 1");
+console.log("23" == 23);
+console.log("23" === 23);
+
+console.log("Part 2");
+console.log(undefined == null);
+console.log(undefined === null);
+
+console.log("Part 3");
+console.log(isNaN("2")); // NaN: Not a Number
+console.log(isNaN(2));
+console.log(isNaN("a"));
+
+console.log("Part 4");
+console.log(typeof null);
